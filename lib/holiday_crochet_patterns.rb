64a5c4bdf70e951428ba54ed5aa8cd5f214f9adf
jrtiff0221 <jrtiff0221@gmail.com>
2019-10-03 20:10:43 -0600

    init project

---
+++ lib/holiday_crochet_patterns.rb
@@ -0,0 +1,6 @@
+require "holiday_crochet_patterns/version"
+
+module HolidayCrochetPatterns
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/holiday_crochet_patterns/version.rb
@@ -0,0 +1,3 @@
+module HolidayCrochetPatterns
+  VERSION = "0.1.0"
+end
